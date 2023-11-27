ba718419330b050c345c762562f076ba540fc7b4
noxknow <chris2769@naver.com>
2023-11-28 00:15:47 +0900

    feat : 과정이 존재하지 않는다면 예외가 발생하는 기능 구현

--- src/main/java/pairmatching/domain/wrapper/CourseLevelMissions.java
+++ src/main/java/pairmatching/domain/wrapper/CourseLevelMissions.java
@@ -5,14 +5,18 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 import static pairmatching.handler.ConstantsHandler.COURSE_LEVEL_MISSIONS_DELIMITER;
+import static pairmatching.handler.ConstantsHandler.ZERO_INDEX;
+import static pairmatching.handler.ErrorHandler.INVALID_COURSE;
 import static pairmatching.handler.ErrorHandler.INVALID_FORMAT;
 
 public class CourseLevelMissions {
 
     private final List<String> values;
+    private final Course course;
 
     private CourseLevelMissions(String inputValue) {
         this.values = validateFormat(inputValue);
+        this.course = validateCourse(values.get(ZERO_INDEX.getValue()));
     }
 
     public static CourseLevelMissions from(String inputValue) {
@@ -28,5 +32,14 @@ public class CourseLevelMissions {
 
         List<String> values = Arrays.asList(inputValue.split(COURSE_LEVEL_MISSIONS_DELIMITER.getWord()));
         return values;
+    }
+
+    private Course validateCourse(String courseName) {
+        try {
+            Course course = Course.getCourse(courseName);
+            return course;
+        } catch (IllegalArgumentException e) {
+            throw INVALID_COURSE.getException();
+        }
     }
 }
